22cc1821c00d8428681a3087ae3a24b977140c68
manuel <manuelfqc@gmail.com>
2024-01-28 10:46:37 -0500

    Agregar archivos de proyecto.

---
+++ LibraryASPMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryASPMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
