ec55ad77d64a4fa35e1396b2ed9138b8f50c032f
manuel <manuelfqc@gmail.com>
2024-01-28 11:38:23 -0500

    [28/01/2024 11:37] set db connection and program settings

--- LibraryASPMVC/Program.cs
+++ LibraryASPMVC/Program.cs
@@ -1,7 +1,21 @@
+using LibraryASPMVC.Data;
+using LibraryASPMVC.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+// Add the connection string to connect the api to the data base 
+builder.Services.AddSqlServer<DB_LibraryContext>(builder.Configuration.GetConnectionString("DBLibraryConnection"));
+// Add runtime changes 
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+//Add service layer
+builder.Services.AddScoped<AuthorService>();
+builder.Services.AddScoped<BookService>();
+builder.Services.AddScoped<BookAuthorInfoService>();
+
 
 var app = builder.Build();
 
